2185140c9dad1432c8efbf319f8bf96e989e4756
unknown <adam.kotynia@gmail.com>
2019-01-19 17:19:05 +0100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID("5c432fa87f41d6a04bfd39aa")
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // })
+
+    db.collection('Users').findOneAndUpdate({
+        name: 'Adam Kotynia'
+    }, {
+        $set: {
+            name: 'gruba karyna'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    })
+
+
+    // db.close();
+});
