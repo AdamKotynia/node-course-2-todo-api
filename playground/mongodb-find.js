0ec5857c0d754f542b39e4153138c328d15b3cda
unknown <adam.kotynia@gmail.com>
2019-01-19 15:01:11 +0100

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c3e6840dcbd773e74b15672')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err)
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err)
+    // });
+
+    db.collection('Users').find({name: "Adam Kotynia"}).toArray().then((docs) => {
+        console.log(docs);
+    }, (err) => {
+        console.log(err);
+    })
+
+    // db.close();
+});
